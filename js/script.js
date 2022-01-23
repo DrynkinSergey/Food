b0c92e212032fc4b1305ad765da048392f8f334f
Sergey <sergsmile29@gmail.com>
2022-01-23 23:59:27 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" -
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,7 +12,30 @@ document.addEventListener('DOMContentLoaded', () => {
         ]
     };
 
+    const films = document.querySelector('.promo__interactive-list');
+    const formAdd = document.querySelector('form.add');
+    const addInput = formAdd.querySelector('.adding__input');
+    const checkbox = formAdd.querySelector('[type="checkbox"]')
+    const trash = document.querySelectorAll('.delete');
 
+
+    const renderFilms = ()=> {
+        films.innerHTML='';
+        movieDB.movies.sort();
+        movieDB.movies.forEach((item,i) => {
+            films.innerHTML+=`<li class="promo__interactive-item">${i+1}. ${movieDB.movies[i]}
+                            <div class="delete"></div>
+                        </li>`
+        })
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                renderFilms();
+            });
+        });
+    }
 
     let promo = document.querySelectorAll('.promo__adv');
     promo.forEach(item => {
@@ -40,22 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
     bg.style.backgroundImage = `url('img/bg.jpg')`;
 
 
-    const renderFilms = ()=> {
-        const films = document.querySelector('.promo__interactive-list');
-        films.innerHTML='';
-        movieDB.movies.sort();
-        movieDB.movies.forEach((item,i) => {
-            films.innerHTML+=`<li class="promo__interactive-item">${i+1}. ${movieDB.movies[i]}
-                            <div class="delete"></div>
-                        </li>`
-        })
-    }
-
-    renderFilms();
-    const formAdd = document.querySelector('form.add');
-    const addInput = formAdd.querySelector('.adding__input');
-    const checkbox = formAdd.querySelector('[type="checkbox"]')
-    const trash = document.querySelectorAll('.delete');
 
     formAdd.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -64,21 +55,16 @@ document.addEventListener('DOMContentLoaded', () => {
             movieDB.movies.push( `${newFilm.substr(0, 20)} ...`);
         } else if(newFilm.length !== 0)
         movieDB.movies.push(newFilm);
-        addInput.value='';
+        event.target.reset();
         renderFilms();
         if (checkbox.checked) {
             console.log("Добавлено в избранные");
         }
 
     });
+    renderFilms()
 
-    trash.forEach((item,i) => {
-        item.addEventListener('click', (e) => {
-            item.parentElement.remove();
-            movieDB.movies.splice(i,1);
 
-        })
-    })
 
 
 })
