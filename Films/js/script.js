86cc2369d1e72b32d2d8635cd0b1e69bffb9ed92
Sergey <sergsmile29@gmail.com>
2022-01-25 00:04:38 +0200

    added features for my tabs

---
+++ Films/js/script.js
@@ -0,0 +1,70 @@
+
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const films = document.querySelector('.promo__interactive-list');
+    const formAdd = document.querySelector('form.add');
+    const addInput = formAdd.querySelector('.adding__input');
+    const checkbox = formAdd.querySelector('[type="checkbox"]')
+    const trash = document.querySelectorAll('.delete');
+    films.style.textTransform ='none';
+
+    const renderFilms = ()=> {
+        films.innerHTML='';
+        movieDB.movies.sort();
+        movieDB.movies.forEach((item,i) => {
+            films.innerHTML+=`<li class="promo__interactive-item">${i+1}. ${movieDB.movies[i]}
+                            <div class="delete"></div>
+                        </li>`
+        })
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                renderFilms();
+            });
+        });
+    }
+
+    let promo = document.querySelectorAll('.promo__adv');
+    promo.forEach(item => {
+        item.remove();
+    })
+    const genre = document.querySelector('.promo__genre');
+    genre.innerHTML = 'Драма'
+    const bg = document.querySelector('.promo__bg');
+    bg.style.backgroundImage = `url('img/bg.jpg')`;
+
+
+
+    formAdd.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let newFilm = addInput.value;
+        if(newFilm.length > 21) {
+            movieDB.movies.push( `${newFilm.substr(0, 20)} ...`);
+        } else if(newFilm.length !== 0)
+        movieDB.movies.push(newFilm);
+        event.target.reset();
+        renderFilms();
+        if (checkbox.checked) {
+            console.log("Добавлено в избранные");
+        }
+
+    });
+    renderFilms()
+
+
+
+
+})
---
+++ Food/js/script.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+        tabs = document.querySelectorAll('.tabheader__item'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabsContent() {
+        tabsContent.forEach(tab => {
+            tab.classList.add('hide');
+            tab.classList.remove('show', 'fade');
+        });
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showCurrentTab(i = 0) {
+        tabsContent[i].classList.add('show','fade');
+        tabsContent[i].classList.remove('hide')
+        tabs[i].classList.add('tabheader__item_active');
+
+    }
+
+        tabsParent.addEventListener('click', (event, i) => {
+            if (event.target && event.target.matches('div.tabheader__item')) {
+                tabs.forEach((tab, i) => {
+                    if (tab == event.target) {
+                        hideTabsContent();
+                        showCurrentTab(i);
+                    }
+                })
+            }
+        });
+
+
+    hideTabsContent();
+    showCurrentTab();
+
+
+});
