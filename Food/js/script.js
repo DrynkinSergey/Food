9084b0d72ec7f13abcdaad2addad0141967cec90
Sergey <sergsmile29@gmail.com>
2022-01-27 01:58:57 +0200

    added modal, modefied, turnoff overflow while modal show, added function close on click anywhere

--- Food/js/script.js
+++ Food/js/script.js
@@ -17,22 +17,22 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showCurrentTab(i = 0) {
-        tabsContent[i].classList.add('show','fade');
+        tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide')
         tabs[i].classList.add('tabheader__item_active');
 
     }
 
-        tabsParent.addEventListener('click', (event, i) => {
-            if (event.target && event.target.matches('div.tabheader__item')) {
-                tabs.forEach((tab, i) => {
-                    if (tab == event.target) {
-                        hideTabsContent();
-                        showCurrentTab(i);
-                    }
-                })
-            }
-        });
+    tabsParent.addEventListener('click', (event, i) => {
+        if (event.target && event.target.matches('div.tabheader__item')) {
+            tabs.forEach((tab, i) => {
+                if (tab == event.target) {
+                    hideTabsContent();
+                    showCurrentTab(i);
+                }
+            })
+        }
+    });
 
 
     hideTabsContent();
@@ -43,22 +43,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const deadline = '2022-01-30';
 
-    function getTimeRemaining (endtime) {
+    function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor(t / (1000*60*60*24)),
-            hours = Math.floor((t / (1000*60*60) % 24)),
-            minutes = Math.floor((t / (1000*60) % 60)),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / (1000 * 60) % 60)),
             seconds = Math.floor((t / 1000) % 60);
 
         return {
-            "total" : t,
-            "days" : days,
-            "hours" : hours,
-            "minutes" : minutes,
-            "seconds" : seconds
+            "total": t,
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
         }
     }
-    function startTimer (selector, endtime) {
+
+    function startTimer(selector, endtime) {
         const timer = document.querySelector(selector),
             days = timer.querySelector('#days'),
             hours = timer.querySelector('#hours'),
@@ -66,19 +67,53 @@ window.addEventListener('DOMContentLoaded', () => {
             seconds = timer.querySelector('#seconds');
 
         function zeroPad(time) {
-            if(time >= 0 && time <10) {
+            if (time >= 0 && time < 10) {
                 return `0${time}`;
             } else return time;
         }
-        function updateTimer () {
+
+        function updateTimer() {
             const timeData = getTimeRemaining(endtime);
             days.innerHTML = zeroPad(timeData.days);
             hours.innerHTML = zeroPad(timeData.hours);
             minutes.innerHTML = zeroPad(timeData.minutes);
             seconds.innerHTML = zeroPad(timeData.seconds);
         }
+
         updateTimer();
         let timerInterval = setInterval(updateTimer, 1000);
     }
-    startTimer('.timer',deadline);
+
+    startTimer('.timer', deadline);
+
+    //Modal
+
+    const modals = document.querySelectorAll('[data-modal]');
+    modals.forEach(btn => {
+        btn.addEventListener('click', showModal);
+    })
+
+    function showModal() {
+        const modal = document.querySelector('.modal'),
+              closeModal = document.querySelector('.modal__close');
+
+        modal.classList.toggle('show');
+        modal.addEventListener('click', (e) => {
+            if(modal.classList.contains('show') && e.target === modal) {
+                modal.classList.remove('show');
+            }
+        })
+        if(modal.classList.contains('show')) {
+            document.body.style.overflow = 'hidden';
+
+        }
+
+
+
+        closeModal.addEventListener('click', () => {
+            modal.classList.remove('show');
+         document.body.style.overflow = '';
+        });
+    }
+
 });
